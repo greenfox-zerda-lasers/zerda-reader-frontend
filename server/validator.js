781d1e75f7e43fa62dc08836bd34aecd3ba000e9
Pracser Tamas <t.pracser@gmail.com>
2017-01-23 17:01:18 +0100

    moday commit

--- server/validator.js
+++ server/validator.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var users = [{
+	email: "bogi@reader.com",
+	password: "bogi",
+}, {
+	email: "gabor@reader.com",
+	password: "gabor",
+}, {
+	email: "tamas@reader.com",
+	password: "tamas",
+}]
+
 function validator(email, password) {
   var validEmail = email.indexOf('@') > -1;
   var validPassword = password.length > 0;
