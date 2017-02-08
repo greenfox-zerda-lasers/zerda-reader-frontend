545c5e2332b3045877762b3d364d4a8c94d8bce4
matheb <mathebogi@gmail.com>
2017-02-08 09:55:49 +0100

    ngResource installed

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
+// Let's require all files that need to be bundled
 
+// Import angular modules:
 require('./lib/angular.js');
 require('./lib/angular-route.min.js');
 require('./lib/angular-animate.min.js');
 require('./lib/angular-mocks.js');
+require('./node_modules/angular-resource/angular-resource.js')
+
+// Import Semantic UI modules (styles):
 require('./node_modules/semantic-ui/dist/semantic.min.css');
 require('./node_modules/semantic-ui/dist/components/accordion.css');
 require('./node_modules/semantic-ui/dist/components/popup.css');
+
+// Import custom Sass styling:
 require('./style/login.scss');
 require('./style/home.scss');
-// require('./node_modules/ngstorage/ngStorage.js');
+
+// Router:
 require('./router.js');
 
+// All the controllers:
 require('./app/login/login.controller.js');
 require('./app/signup/signup.controller.js');
+require('./app/home/home.js');
+
 require('./app/navbar/navbar.controller.js');
 require('./app/sidebar/sidebar.controller.js');
 require('./app/addsubscription/addsubscription.controller.js');
 require('./app/mainlist/mainlist.controller.js');
-
-require('./app/home/home.js');
