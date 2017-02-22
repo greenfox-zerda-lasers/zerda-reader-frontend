691b451b7f99aea08f42e43db8a5996b927a4f26
Gyorgy Gabor <gyorgyg@gmail.com>
2017-02-22 23:13:28 +0100

    add 'use strict' every js file

--- app/login/login.controller.spec.js
+++ app/login/login.controller.spec.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 describe('Sample tests', function () {
   it('has a dummy spec to test 2 + 2', function () {
--- app/navbar/navbar.controller.spec.js
+++ app/navbar/navbar.controller.spec.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 describe('Navbar controller', function () {
   var NavbarController;
